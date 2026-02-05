57efa94798f4b48ccf67fb79da138c1726985a44
Melih7342 <melih.gueldogdu@stud.fh-campuswien.ac.at>
2026-02-05 16:56:30 +0100

    built the engine, which uses channels and routines to scrape asynchronusly

--- internal/engine/engine.go
+++ internal/engine/engine.go
@@ -1,1 +1,37 @@
 package engine
+
+import (
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/Melih7342/parallel-scraper/internal/structs"
+)
+
+func RunEngine(urls []string, workerCount int) []structs.ScrapeResult {
+	jobs := make(chan string, len(urls))
+	results := make(chan structs.ScrapeResult, len(urls))
+
+	var wg sync.WaitGroup
+	client := &http.Client{Timeout: time.Second * 10}
+
+	for w := 1; w <= workerCount; w++ {
+		wg.Add(1)
+		go func(workerID int) {
+			defer wg.Done()
+			StartWorker(workerID, jobs, results, client)
+		}(w)
+	}
+	for _, url := range urls {
+		jobs <- url
+	}
+	close(jobs)
+	wg.Wait()
+	close(results)
+
+	finalResults := make([]structs.ScrapeResult, 0, len(urls))
+	for result := range results {
+		finalResults = append(finalResults, result)
+	}
+	return finalResults
+}
