e642966f0428319227ab6223415b27708e25a615
Melih7342 <melih.gueldogdu@stud.fh-campuswien.ac.at>
2026-02-05 13:59:59 +0100

    wrote first version of title scraper

--- internal/scraper/titleScraper.go
+++ internal/scraper/titleScraper.go
@@ -1,8 +1,38 @@
 package scraper
 
-import "github.com/gocolly/colly"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
 
-func titleScraper() {
-	collector := colly.NewCollector()
+	"github.com/PuerkitoBio/goquery"
+)
 
+func titleScraper(url string) (string, error) {
+	// 1. Request with timeout
+	client := &http.Client{Timeout: 5 * time.Second}
+	resp, err := client.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("status code error: %d %s", resp.StatusCode, resp.Status)
+	}
+
+	// 2. Load html doc
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	// 3. Find the title (<title> or <h1>)
+	title := doc.Find("title").Text()
+	if title == "" {
+		title = doc.Find("h1").First().Text()
+	}
+
+	return strings.TrimSpace(title), nil
 }
