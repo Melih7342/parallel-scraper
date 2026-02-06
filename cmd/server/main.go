01d330f6a0f04833fefe1a31d6367422a29df221
Melih7342 <melih.gueldogdu@stud.fh-campuswien.ac.at>
2026-02-06 10:28:40 +0100

    added gin connection to main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"github.com/Melih7342/parallel-scraper/internal/handler"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	scraperHandler := handler.NewScraperHandler()
+
+	api := r.Group("/api")
+	{
+		api.POST("/scrape/title", scraperHandler.PostScrape)
+	}
+
+	err := r.Run(":8080")
+	if err != nil {
+		return
+	}
+}
